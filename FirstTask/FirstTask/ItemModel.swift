1d143b246ae071b271d52372ad94929bd4d2b690
Mohamed Digwey <mohamed.eldigwey@vodafone.com>
2016-04-17 12:54:40 +0200

    Adding Item model that represent DataModel in JSON

---
+++ FirstTask/FirstTask/ItemModel.swift
@@ -0,0 +1,49 @@
+//
+//  ItemModel.swift
+//  FirstTask
+//
+//  Created by Mohamed Digwey on 4/17/16.
+//  Copyright © 2016 AMIT Org. All rights reserved.
+//
+
+import UIKit
+
+class ItemModel {
+    
+    // MARK: Properties
+    var id: Double
+    var langtitude: Double
+    var latitude: Double
+    var userNumber: String
+    var address: String
+    var userFK: Double
+    
+    
+    // MARK: Initialization
+    //Make it as failable initializer to can return nil when initialization
+    init?(id: Double, langtitude: Double, latitude: Double, userNumber: String, address: String, userFK: Double) {
+        // Initialize stored properties.
+        self.id = id
+        self.langtitude = langtitude
+        self.latitude = latitude
+        self.userNumber = userNumber
+        self.address = address
+        self.userFK = userFK
+        
+        if(id < 0){
+            return nil
+        }
+    }
+    
+    //Using this initializer to parse JSON from NSDictionary to data model
+    init(dic: NSDictionary)
+    {
+        self.id = (dic["id"] as? Double)!
+        self.langtitude = (dic["langtitude"] as? Double)!
+        self.latitude = (dic["latitude"] as? Double)!
+        self.userNumber = (dic["userNumber"] as? String)!
+        self.address = (dic["address"] as? String)!
+        self.userFK = (dic["userFK"] as? Double)!
+    }
+
+}
