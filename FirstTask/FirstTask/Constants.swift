193ab60425b11ccd18405615d2db50b7d315e278
Mohamed Digwey <mohamed.eldigwey@vodafone.com>
2016-04-18 09:40:54 +0200

    Isolate URL variables in operated file

---
+++ FirstTask/FirstTask/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  FirstTask
+//
+//  Created by Mohamed Digwey on 4/18/16.
+//  Copyright © 2016 AMIT Org. All rights reserved.
+//
+
+import UIKit
+let httpURL:String = "http://amit-learning.com/parkForMe/index.php"
+
+
+class Constants{
+    
+}
